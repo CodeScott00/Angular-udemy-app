9ab5a20fd1902f308abeb22e788b77c2cc603e45
Scott Dougal <scott.dougal@envoyat.com>
2023-03-16 10:55:37 +1100

    added multiple components and played around with styling at different levels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 @NgModule({
   //components need imported here on creation. then imported too
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   imports: [BrowserModule], // may need more imports here shortly
   providers: [],
   bootstrap: [AppComponent],
