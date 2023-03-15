cf4050acc8a690a3683523e969c376637e5381cc
Scott Dougal <scott.dougal@envoyat.com>
2023-03-15 16:37:07 +1100

    end of day commit, assignment to follow tomorrow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   //components need imported here on creation. then imported too
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule], // may need more imports here shortly
   providers: [],
   bootstrap: [AppComponent],
