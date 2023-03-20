121846d7ed14e9e3944f993c29886a1b1c407a9a
Scott Dougal <scott.dougal@envoyat.com>
2023-03-20 11:16:36 +1100

    assignment done

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -8,16 +8,15 @@ import { Component } from '@angular/core';
 export class PasswordComponent {
   passwordDisplay = false;
   buttonClickCount = [];
-  buttonClickNumber = 0;
 
   updatePasswordDisplay() {
     this.passwordDisplay = !this.passwordDisplay;
     this.buttonClickCount.push(this.buttonClickCount.length + 1);
   }
 
-  incrementButtonClick() {
-    if (this.passwordDisplay) {
-      return (this.buttonClickNumber += 1);
-    }
-  }
+  // blueIfGreaterThanFive(buttonClickCount) {
+  //   if(this.buttonClickCount.length < 5){
+  //     return
+  //   }
+  // }
 }
